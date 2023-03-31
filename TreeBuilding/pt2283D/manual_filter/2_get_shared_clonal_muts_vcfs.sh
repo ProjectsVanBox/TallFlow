cabf6690992b94ac02e877fed1bbbf0e70f26a12
rhagelaar <ricohagelaar@gmail.com>
2023-03-31 11:57:47 +0200

    Added scripts for treebuilding for pt2283D

---
+++ TreeBuilding/pt2283D/manual_filter/2_get_shared_clonal_muts_vcfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mytreedir="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/TreeBuilding/pt2283D/manual_filter"
+myinfofile="${mytreedir}/shared_clonal_muts_info.txt"
+mysmurfvcf="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/PTATO_Dev/pt2283D/intermediate/short_variants/SMuRF/pt2283D/230307_HMF1931_pt2283D.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.SMuRF.filtered.vcf
+"
+
+if [ ! -d ${mytreedir}/muts_vcf ]; then
+  mkdir -p ${mytreedir}/muts_vcf
+fi
+
+
+grep "^#" ${mysmurfvcf} > ${mytreedir}/header.vcf
+
+while read line; do
+  line_grep=$(echo ${line} | awk '{ print $2}' )
+  nmuts=$(echo ${line} | awk '{ print $1}' )
+  mysamples=$(echo ${line} | cut -f 2 -d'=' | tr ',' '_' | sed 's/pt2283D-DX1BM-//g')
+  mysharedmutsvcf=${mytreedir}/muts_vcfs/${nmuts}_${mysamples}.vcf
+  cat ${mytreedir}/header.vcf > ${mysharedmutsvcf}
+  grep ${line_grep} ${mysmurfvcf} | awk '{ if (length($4) == 1 && length($5) == 1) { print $0} }'>> ${mysharedmutsvcf}
+done < ${myinfofile}
---
+++ TreeBuilding/pt2283D/withoutBulk/get_unique_braches.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+myptatodir="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/PTATO_Dev/pt2322/snvs/pt2322"
+mytreedir="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/TreeBuilding/pt2322/withoutBulk/branches_vcfs"
+mysmurfvcf="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/PTATO_Dev/pt2322/intermediate/short_variants/SMuRF/pt2322/220919_HMFreg1764_pt2322.SMuRF.filtered.vcf"
+
+if [ ! -d ${mytreedir} ]; then
+  mkdir -p ${mytreedir}
+fi
+
+for VCF in ${myptatodir}/*/*.vcf.gz; do 
+    SAMPLE=$( echo ${VCF} | grep -oP "TALL\d+.ptato" | cut -f1 -d'.')
+    if [ ! -z ${SAMPLE} ]; then
+        zgrep "^#" ${mysmurfvcf} > ${mytreedir}/${SAMPLE}_branch.vcf
+        grep -f <( zgrep -P ";CLONAL_SAMPLES=1;" ${VCF} | grep -P ";ABSENT_SAMPLES=(11|12|13)" | cut -f 1,2 ) ${mysmurfvcf} >> ${mytreedir}/${SAMPLE}_branch.vcf
+    fi
+    
+#    /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/PTATO/pt2322/snvs/pt2322/220919_HMFreg1764_pt2322_pt2322-DX1BM-ALLBULK/220919_HMFreg1764_pt2322_pt2322-DX1BM-ALLBULK.ptato.filtered.vcf.gz
+    
+done
+
+# Search for shared "clonal" mutations
+#zgrep -v "^#" ${myptatodir}/*/*.vcf.gz | \
+# cut -f 1,2,8 | \
+# cut -f 2- -d ':' | \
+# sort | \
+# uniq -c | \
+# grep -P ";CLONAL_SAMPLES=1;" | \
+# sort | \
+# uniq -c | \
+# sort -k1n \
+# > ${mytreedir}/unique_clonal_muts_info.txt
