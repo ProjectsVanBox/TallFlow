4c712e63c3b18908ad363daa3e11a4482e31c2b9
rhagelaar <ricohagelaar@gmail.com>
2023-07-04 10:32:13 +0200

    Driver filtering for pt344

---
+++ Drivers/pt344/grep_moderate_high.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VCF=$1
+OUT=$(basename ${VCF})
+
+OUT=${OUT/%.vcf/.drivers.vcf}
+
+grep -P "^#|MODERATE|HIGH" ${VCF} > /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/Drivers/pt344/${OUT}
