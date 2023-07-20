d4ac7f7e0f69fb84c6e52cc20fbc1378adb268d4
Vera Poort <vpoort@hpcs04.op.umcutrecht.nl>
2023-07-20 16:30:56 +0200

    big clean up of redundant scripts

--- TreeBuilding/pt2283D/manual_filter/3_get_shared_clonal_muts_coords.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mytreedir="/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/TreeBuilding/pt2283D/manual_filter"
-
-cat ${mytreedir}/muts_vcfs/*.vcf | \
-grep -v "^#" | \
-cut -f 1,2 | \
-sort | \
-uniq > ${mytreedir}/shared_clonal_muts_coords.txt
