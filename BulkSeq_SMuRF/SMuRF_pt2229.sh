ca802c65075f9805ad0bf4b9147c2300b18fa2be
rhagelaar <ricohagelaar@gmail.com>
2024-01-19 11:36:32 +0100

    Cleaned up scripts for running SMuRF for pt2229

--- BulkSeq_SMuRF/SMuRF_pt2229.sh
+++ BulkSeq_SMuRF/SMuRF_pt2229.sh
@@ -8,19 +8,19 @@
 
 cd /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/BulkSeq_SMuRF/pt2229
 
-. /hpc/pmc_vanboxtel/tools/ToolsVanBox/SMuRF-3.0.0/venv_3.6/bin/activate
-  /hpc/local/CentOS7/pmc_vanboxtel/bin/samtools-bcftools-htslib-1.0_x64-linux/bin/bgzip -c /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229/221026_HMFreg1816_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf > /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229/221026_HMFreg1816_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz 
-  /hpc/local/CentOS7/pmc_vanboxtel/bin/samtools-bcftools-htslib-1.0_x64-linux/bin/tabix /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229/221026_HMFreg1816_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz 
+/hpc/local/CentOS7/pmc_vanboxtel/bin/samtools-bcftools-htslib-1.0_x64-linux/bin/bgzip -c /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229_inclDP/230124_HMFreg1891_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf > /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229_inclDP/230124_HMFreg1891_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz
+/hpc/local/CentOS7/pmc_vanboxtel/bin/samtools-bcftools-htslib-1.0_x64-linux/bin/tabix /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229_inclDP/230124_HMFreg1891_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz
 
- python /hpc/pmc_vanboxtel/tools/ToolsVanBox/SMuRF-3.0.0/SMuRF.py \
-  -i /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229/221026_HMFreg1816_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz \
+singularity exec -B /hpc /hpc/local/CentOS7/pmc_vanboxtel/singularity_cache/vanboxtelbioinformatics-smurf-3.0.2.img python /smurf/SMuRF.py \
+  -i /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/VCFS/pt2229_inclDP/230124_HMFreg1891_pt2229.vcf.filtered_variants_dbnsfp_CosmicCodingMuts_gonl.snps_indels.r5.liftover.hg38.sorted.vcf.gz \
   -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BAMS/pt2229/pt2229-DX1BM-ALLBULK_dedup.bam \
   -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/BAMS/pt2229/pt2229-DX1BM-ALLBULK-DNCD1aNeg_dedup.bam \
   -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/BAMS/pt2229/pt2229-DX1BM-ALLBULK-DNCD1aPos_dedup.bam \
   -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/BAMS/pt2229/pt2229-DX1BM-ALLBULK-iSPCD4_dedup.bam \
+  -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/BAMS/pt2229/pt2229-DX1BM-ALLBULK-DP_dedup.bam \
   -b /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/BULK_SEQ/BAMS/pt2229/pt2229-DX1BM-MSCBULK_dedup.bam \
   -n pt2229-DX1BM-MSCBULK \
-  -c /hpc/pmc_vanboxtel/projects/TallFlow/2_Code/BulkSeq_SMuRF/SMuRF_config.ini
+  -c /hpc/pmc_vanboxtel/projects/TallFlow/2_Code/BulkSeq_SMuRF/SMuRF_config15x.ini
 
 #bash /hpc/pmc_vanboxtel/tools/ToolsVanBox/SMuRF-3.0.0/scripts/split_mutation_type.sh /hpc/pmc_vanboxtel/projects/Ageline/2_Code/2_scripts/10817/10817.filtered_variants_snpEff_snpSift_Cosmicv89_SMuRF_filtered.vcf
 
