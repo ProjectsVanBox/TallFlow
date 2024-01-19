a2d82789067405c9b706a583cc762727d2dd48ce
rhagelaar <ricohagelaar@gmail.com>
2024-01-19 12:03:50 +0100

    Added code for haplotype caller

---
+++ HaplotypeCaller/pt1180_pt1909/1_create_bed_from_ptato_vcfs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load bedtools
+
+#### pt1180_pt1909
+patientID="pt1180_pt1909"
+
+cd "/hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt1180_pt1909/"
+
+for VCF in /hpc/pmc_vanboxtel/projects/TallFlow/1_Input/HaplotypeCaller/pt1180_pt1909/*.vcf; do
+	echo ${VCF}
+	grep -v "^#" ${VCF} | awk '{print $1"\t"$2-1"\t"$2}' >> /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt1180_pt1909/${patientID}_interval_list.bed
+done
+
+grep "^@" /hpc/pmc_vanboxtel/resources/homo_sapiens.GRCh38.GATK.illumina/genome.interval_list > /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt1180_pt1909/${patientID}_interval_list_merged.interval_list
+
+bedtools sort -i /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt1180_pt1909/${patientID}_interval_list.bed | bedtools merge | awk '{print $0"\t+\t."}' >> /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt1180_pt1909/${patientID}_interval_list_merged.interval_list
+
---
+++ HaplotypeCaller/run_NF_IAP_pt3291.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=10:0:0
+#SBATCH --mem=25G
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=r.hagelaar@prinsesmaximacentrum.nl
+
+CONFIG=run_NF_IAP_pt3291.config
+
+module load Java
+
+/hpc/pmc_vanboxtel/tools/external/nextflow_v20.03.0-edge.5288/nextflow run /hpc/pmc_vanboxtel/tools/ToolsVanBox/NF-IAP_v1.3.0/nf-iap.nf \
+ -c ${CONFIG} --out_dir /hpc/pmc_vanboxtel/projects/TallFlow/3_Output/HaplotypeCaller/pt3291/ -profile slurm -resume
